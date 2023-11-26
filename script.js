2fec427f012137d0f1eb8fe846b27a0e77fe83b2
BASMaDSBAS <152096923+BASMaDSBAS@users.noreply.github.com>
2023-11-26 21:18:34 +0100

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-// Générer un nombre aléatoire entre 1 et 100
-const secretNumber = Math.floor(Math.random() * 100) + 1;
+// Données du jeu
+const gameData = [
+    { word: 'Chien', image: 'https://raw.githubusercontent.com/VotreNomUtilisateur/NomDuDepot/main/images/chien.jpg' },
+    { word: 'Chat', image: 'https://raw.githubusercontent.com/VotreNomUtilisateur/NomDuDepot/main/images/chat.jpg' },
+    // Ajoutez d'autres données selon vos besoins
+];
 
-let attempts = 0;
+let currentRound = 0;
+let score = 0;
 
-function checkGuess() {
-    // Récupérer la valeur entrée par l'utilisateur
-    const userGuess = document.getElementById('user-guess').value;
+// Fonction pour vérifier la réponse
+function checkAnswer() {
+    const userInput = document.getElementById('word-input').value.toLowerCase();
+    const correctAnswer = gameData[currentRound].word.toLowerCase();
+    const resultElement = document.getElementById('result');
 
-    // Vérifier si la valeur est correcte
-    if (userGuess == secretNumber) {
-        displayMessage(`Bravo ! Vous avez deviné le nombre en ${attempts} tentatives.`);
-    } else if (userGuess < secretNumber) {
-        displayMessage('Le nombre est plus grand. Essayez à nouveau.');
+    if (userInput === correctAnswer) {
+        resultElement.textContent = 'Correct !';
+        score++;
     } else {
-        displayMessage('Le nombre est plus petit. Essayez à nouveau.');
+        resultElement.textContent = 'Incorrect. La réponse était ' + correctAnswer;
     }
-
-    // Incrémenter le nombre de tentatives
-    attempts++;
 }
 
-function displayMessage(message) {
-    // Afficher le message
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = message;
+// Fonction pour passer à la question suivante
+function nextRound() {
+    const wordInput = document.getElementById('word-input');
+    const resultElement = document.getElementById('result');
+
+    // Réinitialiser le champ de saisie et le résultat
+    wordInput.value = '';
+    resultElement.textContent = '';
+
+    // Passer à la question suivante
+    currentRound++;
+
+    // Vérifier si toutes les questions ont été posées
+    if (currentRound < gameData.length) {
+        // Mettre à jour l'image
+        const imageContainer = document.getElementById('image-container');
+        const currentImage = gameData[currentRound].image;
+        imageContainer.innerHTML = `<img src="${currentImage}" alt="Image">`;
+    } else {
+        // Fin du jeu
+        endGame();
+    }
+}
+
+// Fonction pour terminer le jeu
+function endGame() {
+    alert(`Fin du jeu ! Votre score est de ${score} sur 10.`);
+    // Vous pouvez ajouter d'autres actions ici, comme réinitialiser le jeu.
 }
