fb4326393642958089e8d55f30a138963582d72b
BASMaDSBAS <152096923+BASMaDSBAS@users.noreply.github.com>
2023-11-27 00:43:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 // Données du jeu
-const gameData = [
-    { word: 'un', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/1.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/2.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/3.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/4.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/5.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/6.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/7.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/8.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/9.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/10.JPG' },
-    { word: 'deux', image: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu/main/photo/11.JPG' },
-    // Ajoutez d'autres données selon vos besoins
-];
-
+let gameData = [];
 let currentRound = 0;
 let score = 0;
+let debutJeu;
 
-// Fonction pour vérifier la réponse
-function checkAnswer() {
-    const userInput = document.getElementById('word-input').value.toLowerCase();
-    const correctAnswer = gameData[currentRound].word.toLowerCase();
-    const resultElement = document.getElementById('result');
-
-    if (userInput === correctAnswer) {
-        resultElement.textContent = 'Correct !';
-        score++;
-    } else {
-        resultElement.textContent = 'Incorrect. La réponse était ' + correctAnswer;
-    }
+// Fonction pour commencer une nouvelle manche
+function startGame() {
+    gameData = chargerPhotosDepuisGitHub();
+    melangerPhotos();
+    debutJeu = Date.now();
+    afficherQuestion();
 }
 
-// Fonction pour passer à la question suivante
-function nextRound() {
+// Fonction pour charger les photos depuis GitHub (à remplacer par une vraie implémentation)
+function chargerPhotosDepuisGitHub() {
+    // Ici, vous devrez implémenter la logique pour récupérer les photos depuis GitHub.
+    // Remplacez ce tableau statique par votre propre mécanisme pour récupérer les données.
+    return [
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/1.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/2.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/3.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/4.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/5.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/6.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/7.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/8.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/9.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/10.JPG', mot: 'un' },
+        { url: 'https://raw.githubusercontent.com/BASMaDSBAS/jeu-bda/main/11.JPG', mot: 'un' },
+        // Ajoutez d'autres données selon vos besoins
+    ];
+}
+
+// Fonction pour mélanger les photos
+function melangerPhotos() {
+    gameData = shuffleArray(gameData);
+}
+
+// Fonction pour afficher une question
+function afficherQuestion() {
+    const imageContainer = document.getElementById('image-container');
     const wordInput = document.getElementById('word-input');
     const resultElement = document.getElementById('result');
 
@@ -40,14 +47,10 @@ function nextRound() {
     wordInput.value = '';
     resultElement.textContent = '';
 
-    // Passer à la question suivante
-    currentRound++;
-
     // Vérifier si toutes les questions ont été posées
     if (currentRound < gameData.length) {
         // Mettre à jour l'image
-        const imageContainer = document.getElementById('image-container');
-        const currentImage = gameData[currentRound].image;
+        const currentImage = gameData[currentRound].url;
         imageContainer.innerHTML = `<img src="${currentImage}" alt="Image">`;
     } else {
         // Fin du jeu
@@ -55,8 +58,51 @@ function nextRound() {
     }
 }
 
+// Fonction pour vérifier la réponse
+function checkAnswer() {
+    const userInput = document.getElementById('word-input').value.toLowerCase();
+    const correctAnswer = gameData[currentRound].mot.toLowerCase();
+    const resultElement = document.getElementById('result');
+
+    if (userInput === correctAnswer) {
+        resultElement.textContent = 'Correct !';
+        score++;
+    } else {
+        resultElement.textContent = `Incorrect. La réponse était ${correctAnswer}`;
+    }
+}
+
+// Fonction pour passer à la question suivante
+function nextRound() {
+    currentRound++;
+
+    // Passer à la question suivante
+    afficherQuestion();
+
+    // Vérifier si toutes les questions ont été posées
+    if (currentRound === gameData.length) {
+        // Fin du jeu
+        endGame();
+    }
+}
+
 // Fonction pour terminer le jeu
 function endGame() {
-    alert(`Fin du jeu ! Votre score est de ${score} sur 10.`);
+    const finJeu = Date.now();
+    const tempsJeu = (finJeu - debutJeu) / 1000; // Convertir en secondes
+
+    alert(`Fin du jeu ! Votre score est de ${score} sur ${gameData.length}. Temps total : ${tempsJeu} secondes.`);
     // Vous pouvez ajouter d'autres actions ici, comme réinitialiser le jeu.
 }
+
+// Fonction pour mélanger un tableau de manière aléatoire
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Démarrer le jeu lors du chargement de la page
+startGame();
